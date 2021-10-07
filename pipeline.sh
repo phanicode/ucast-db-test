d352c2d3d9cc4f772dc4198191467a16cdab421d
phanicode <63126072+phanicode@users.noreply.github.com>
2021-10-07 15:33:51 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,27 +1,28 @@
- git clone https://github.com/phanicode/ucast-db-test.git
- git config user.name 'phanicode'
- git config user.email 'phaniv@email.arizona.edu'
- cd ucast-db-test
- git pull origin
- git checkout archive
- git checkout main -- .
- git commit -m "copy files from main to archive"
- Y=$(date -d "12 hours ago" '+%Y')
- m=$(date -d "12 hours ago" '+%m')
- d=$(date -d "12 hours ago" '+%d')
- echo $Y $m $d
- echo ALMA/$Y-$m
- git rm --ignore-unmatch ALMA/$Y-$m* 
- git rm --ignore-unmatch APEX/$Y-$m*
- git rm --ignore-unmatch GLT/$Y-$m*
- git rm --ignore-unmatch JCMT/$Y-$m*
- git rm --ignore-unmatch KP/$Y-$m*
- git rm --ignore-unmatch LMT/$Y-$m*
- git rm --ignore-unmatch PDB/$Y-$m*
- git rm --ignore-unmatch PV/$Y-$m*
- git rm --ignore-unmatch SMA/$Y-$m*
- git rm --ignore-unmatch SMT/$Y-$m*
- git rm --ignore-unmatch SPT/$Y-$m*
- git add .
- git commit --allow-empty -m "removed last month"  
- git push --all --force --set-upstream origin
+git clone https://github.com/phanicode/ucast-db-test.git
+git config user.name 'phanicode'
+git config user.email 'phaniv@email.arizona.edu'
+cd ucast-db-test
+git pull origin
+git checkout archive
+git checkout main -- .
+git commit -m "copy files from main to archive"
+git push --all --force --set-upstream origin
+Y=$(date -d "12 hours ago" '+%Y')
+m=$(date -d "12 hours ago" '+%m')
+d=$(date -d "12 hours ago" '+%d')
+echo $Y $m $d
+echo ALMA/$Y-$m
+git rm --ignore-unmatch ALMA/$Y-$m* 
+git rm --ignore-unmatch APEX/$Y-$m*
+git rm --ignore-unmatch GLT/$Y-$m*
+git rm --ignore-unmatch JCMT/$Y-$m*
+git rm --ignore-unmatch KP/$Y-$m*
+git rm --ignore-unmatch LMT/$Y-$m*
+git rm --ignore-unmatch PDB/$Y-$m*
+git rm --ignore-unmatch PV/$Y-$m*
+git rm --ignore-unmatch SMA/$Y-$m*
+git rm --ignore-unmatch SMT/$Y-$m*
+git rm --ignore-unmatch SPT/$Y-$m*
+git add .
+git commit --allow-empty -m "removed last month"  
+git push --all --force --set-upstream origin
