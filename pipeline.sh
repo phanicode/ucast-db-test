cef37d344eebb62baa1510a77ddad2ab63896b64
phanicode <63126072+phanicode@users.noreply.github.com>
2021-10-07 15:17:53 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,22 +1,17 @@
-# git clone https://github.com/phanicode/ucast-db-test.git
-# cd ucast-db-test
-# git checkout archive
-# git merge main -m "merging main"
-
+git checkout archive
+git checkout main -- .
+git commit -m "copy files from main to archive"
 Y=$(date -d "12 hours ago" '+%Y')
 m=$(date -d "12 hours ago" '+%m')
 d=$(date -d "12 hours ago" '+%d')
 echo $Y $m $d
 echo ALMA/$Y-$m
-# git add .
-# git commit --allow-empty -m "merged main"
-git checkout main
 git rm --ignore-unmatch ALMA/$Y-$m* 
 git rm --ignore-unmatch APEX/$Y-$m*
 git rm --ignore-unmatch GLT/$Y-$m*
 git rm --ignore-unmatch JCMT/$Y-$m*
 git rm --ignore-unmatch KP/$Y-$m*
-git rm --ignore-unmatch LMT/$Y-$m*
+git rm --ignore-unmatch LMT/$Y-$m*bash
 git rm --ignore-unmatch PDB/$Y-$m*
 git rm --ignore-unmatch PV/$Y-$m*
 git rm --ignore-unmatch SMA/$Y-$m*
@@ -25,5 +20,3 @@ git rm --ignore-unmatch SPT/$Y-$m*
 git add .
 git commit --allow-empty -m "removed last month"  
 git push --all --force --set-upstream origin
-# cd ..
-# rm -rf ucast-db-tests
