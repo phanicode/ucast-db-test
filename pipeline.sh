a8601dc898c229783170ece9b7f3792928530bdc
phanicode <phaniv@email.arizona.edu>
2021-10-07 15:29:00 -0700

    copy test

--- pipeline.sh
+++ pipeline.sh
@@ -1,29 +1,27 @@
-git clone https://github.com/phanicode/ucast-db-test.git
-cd ucast-db-test
-git checkout archive
-git merge main -m "merging main"
-
-Y=$(date -d "12 hours ago" '+%Y')
-m=$(date -d "27 days ago" '+%m')
-d=$(date -d "12 hours ago" '+%d')
-echo $Y $m $d
-echo ALMA/$Y-$m
-git add .
-git commit --allow-empty -m "merged main"
-git checkout main
-git rm --ignore-unmatch ALMA/$Y-$m* 
-git rm --ignore-unmatch APEX/$Y-$m*
-git rm --ignore-unmatch GLT/$Y-$m*
-git rm --ignore-unmatch JCMT/$Y-$m*
-git rm --ignore-unmatch KP/$Y-$m*
-git rm --ignore-unmatch LMT/$Y-$m*
-git rm --ignore-unmatch PDB/$Y-$m*
-git rm --ignore-unmatch PV/$Y-$m*
-git rm --ignore-unmatch SMA/$Y-$m*
-git rm --ignore-unmatch SMT/$Y-$m*
-git rm --ignore-unmatch SPT/$Y-$m*
-git add .
-git commit --allow-empty -m "removed last month"  
-git push --all --force --set-upstream origin
-cd ..
-rm -rf ucast-db-tests
+ git clone https://github.com/phanicode/ucast-db-test.git
+ git config user.name 'phanicode'
+ git config user.email 'phaniv@email.arizona.edu'
+ cd ucast-db-test
+ git pull origin
+ git checkout archive
+ git checkout main -- .
+ git commit -m "copy files from main to archive"
+ Y=$(date -d "12 hours ago" '+%Y')
+ m=$(date -d "12 hours ago" '+%m')
+ d=$(date -d "12 hours ago" '+%d')
+ echo $Y $m $d
+ echo ALMA/$Y-$m
+ git rm --ignore-unmatch ALMA/$Y-$m* 
+ git rm --ignore-unmatch APEX/$Y-$m*
+ git rm --ignore-unmatch GLT/$Y-$m*
+ git rm --ignore-unmatch JCMT/$Y-$m*
+ git rm --ignore-unmatch KP/$Y-$m*
+ git rm --ignore-unmatch LMT/$Y-$m*
+ git rm --ignore-unmatch PDB/$Y-$m*
+ git rm --ignore-unmatch PV/$Y-$m*
+ git rm --ignore-unmatch SMA/$Y-$m*
+ git rm --ignore-unmatch SMT/$Y-$m*
+ git rm --ignore-unmatch SPT/$Y-$m*
+ git add .
+ git commit --allow-empty -m "removed last month"  
+ git push --all --force --set-upstream origin
